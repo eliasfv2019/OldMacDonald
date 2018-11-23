f33c927d4645b75353dfa1b15135b06bd03c8542
eliasfv2019 <32773105+eliasfv2019@users.noreply.github.com>
2018-11-23 12:59:19 -0400

    Add files via upload

--- Cow.java
+++ Cow.java
@@ -1,4 +1,15 @@
-class Cow //implements Animal 
-{     
-     //your code here
+class Cow  implements Animal//implements Animal 
+{   
+  private String myType, mySound;
+  public Cow() {
+    myType = "cow";
+    mySound = "Moooooooooooooooooooooooooooooooooooooooooooooooooo!";
+  }
+  public String getType() {
+    return myType;
+  }
+  public String getSound() {
+    return mySound;
+  }
 }
+//your code here
